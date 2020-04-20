3e9538ea53d6fc76639deee1b46c9416ee267621
SeanLewisIRE <slewis100@gmail.com>
2020-04-20 17:22:24 +0100

    added 'manage categories' page to manage all categories functionality

--- app.py
+++ app.py
@@ -8,20 +8,24 @@ app.config["MONGO_DBNAME"] = 'recipie_app'
 app.config["MONGO_URI"] = 'mongodb+srv://root:NecfIPUD5PTBlYMe@practicecluster-tjxfj.mongodb.net/recipie_app?retryWrites=true&w=majority'
 mongo = PyMongo(app)
 
+
 @app.route('/')
 def recipes_page():
     recipes = mongo.db.recipes.find()
     return render_template("recipes.html", recipes=recipes)
+
 
 @app.route('/get_recipe/<recipe_id>')
 def get_recipe(recipe_id):
     _recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template('recipe.html', recipe=_recipe)
 
+
 @app.route('/add_recipe')
 def add_recipe():
     categories = mongo.db.meal_category.find()
     return render_template("addrecipe.html", categories=categories)
+
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
@@ -29,12 +33,13 @@ def insert_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('recipes_page'))
 
+
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     categories = mongo.db.meal_category.find()
     _recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
 
-    return render_template('editrecipe.html', recipe=_recipe, categories = categories)
+    return render_template('editrecipe.html', recipe=_recipe, categories=categories)
 
 
 @app.route('/update_recipe/<recipe_id>', methods=['POST'])
@@ -52,15 +57,38 @@ def update_recipe(recipe_id):
     return redirect(url_for('recipes_page'))
 
 
+@app.route('/manage_categories')
+def manage_categories():
+    category = mongo.db.meal_category.find()
+    return render_template('managecategories.html', categories=category)
+
+@app.route('/edit_category/<category_id>')
+def edit_category(category_id):
+    _category = mongo.db.meal_category.find_one({"_id": ObjectId(category_id)})
+    return render_template('editcategory.html', category=_category)
+
+
+@app.route('/update_category/<category_id>', methods=['POST'])
+def update_category(category_id):
+    categories = mongo.db.meal_category
+    categories.update({'_id': ObjectId(category_id)},
+                   {
+        'category_name': request.form.get('category_name')
+    })
+    return redirect(url_for('manage_categories'))
+
+
 @app.route('/add_category')
 def add_category():
     return render_template("addcategory.html")
+
 
 @app.route('/insert_category', methods=["POST"])
 def insert_category():
     categories = mongo.db.meal_category
     categories.insert_one(request.form.to_dict())
-    return redirect(url_for('recipes_page'))
+    return redirect(url_for('manage_categories'))
+
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(
