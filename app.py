dedad8eea9df9bf39dd24aa5458470e66d492cd7
SeanLewisIRE <slewis100@gmail.com>
2020-04-23 11:37:19 +0100

    added _blank to runnerbeans link on mainpage

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def delete_category(category_id):
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(
-        os.getenv('PORT', '5000')), debug=True)
+        os.getenv('PORT', '5000')), debug=False)
