62b849731382186a26a9d7125300a63fc9bc9cae
SeanLewisIRE <slewis100@gmail.com>
2020-04-23 11:02:24 +0100

    JS test to remove console error.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,15 +1,15 @@
 
-function createList(arr) {
-    let list = "<ul>"
-    for (let i = 0; i < arr.length; i++) {
-        list += `<li>${arr[i]}</li>`
-    }
-    list += "</ul>"
-    return list
-}
+// function createList(arr) {
+//     let list = "<ul>"
+//     for (let i = 0; i < arr.length; i++) {
+//         list += `<li>${arr[i]}</li>`
+//     }
+//     list += "</ul>"
+//     return list
+// }
 
-let ingredientsArr = ingredientsTotal.split(/\./);
-let instructionsArr = instructionsTotal.split(/\./);
-document.querySelector(".ingredients").innerHTML = createList(ingredientsArr);
-document.querySelector(".instructions").innerHTML = createList(instructionsArr);
+// let ingredientsArr = ingredientsTotal.split(/\./);
+// let instructionsArr = instructionsTotal.split(/\./);
+// document.querySelector(".ingredients").innerHTML = createList(ingredientsArr);
+// document.querySelector(".instructions").innerHTML = createList(instructionsArr);
 
